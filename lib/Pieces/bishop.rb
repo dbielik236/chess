375be4b2628be0921a3c8279714702461c137064
David Bielik <davidbielik@gmail.com>
2022-04-05 07:58:57 -0400

    rough created bishop class

---
+++ lib/Pieces/bishop.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Manages the color and legal moves of the bishop
+
+class Bishop
+  attr_accessor :color
+  attr_accessor :icon
+  attr_accessor :move_count
+
+  def initialize(color, icon)
+    @color = color
+    @icon = icon
+    @move_count = 0
+  end
+
+  def legal_move?(start, finish)
+    # horizontal, vertical, diagonal
+  end
+end
