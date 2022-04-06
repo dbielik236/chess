c2c65e3a1745e5d7d737e0d3d4a438555350b23a
David Bielik <davidbielik@gmail.com>
2022-04-06 07:57:41 -0400

    display board is operational

--- lib/board.rb
+++ lib/board.rb
@@ -8,19 +8,38 @@ class Board
   attr_accessor :grid, :starting_piece, :ending_piece
 
   def initialize
+    @color = 46
     @white_rook = Rook.new('white', '♖')
     @black_rook = Rook.new('black', '♜')
     @starting_piece = nil
     @ending_piece = nil
     @grid = [
-      [Square.new('8a', @white_rook.name), eight_b = Square.new('8b'), eight_c = Square.new('8c'), eight_d = Square.new('8d'), eight_e = Square.new('8e'), eight_f = Square.new('8f'), eight_g = Square.new('8g'), eight_h = Square.new('8h')]
+      [Square.new('8a', @white_rook), Square.new('8b', @black_rook), eight_c = Square.new('8c'), eight_d = Square.new('8d'), eight_e = Square.new('8e'), eight_f = Square.new('8f'), eight_g = Square.new('8g'), eight_h = Square.new('8h')], 
+      [Square.new('8a', @white_rook), Square.new('8b'), eight_c = Square.new('8c'), eight_d = Square.new('8d'), eight_e = Square.new('8e'), eight_f = Square.new('8f'), eight_g = Square.new('8g'), eight_h = Square.new('8h')]
     ]
+  end
+
+  # color 46 is light blue, color 40 is black
+  def switch_color
+    if @color == 46
+      @color = 40
+    else @color = 46
+    end
   end
 
   def display
     p 'a  b  c  d  e  f  g  h'
-    @grid[0].each do |item|
-      puts item.piece
+    @grid.each do |row|
+      row.each do |square|
+        if square.piece.nil?
+          print "\e[#{@color}m#{"   "}\e[0m"
+        else
+          print "\e[#{@color}m#{" #{square.piece.icon} "}\e[0m"
+        end
+        switch_color
+      end
+      puts "\n"
+      switch_color
     end
   end
 end
