6501dcadf7ef6ae12b79ccc35a469691ef87a845
David Bielik <davidbielik@gmail.com>
2022-04-07 09:28:33 -0400

    added more prompts and replace piece method in board class

--- lib/prompts.rb
+++ lib/prompts.rb
@@ -18,4 +18,16 @@ module Display
   def color_error_prompt
     puts 'Please choose a color by entering the letter b or w.'
   end
+
+  def starting_piece_prompt
+    puts 'Please enter the location of the piece you\'d like to move. (for example, a1)'
+  end
+
+  def incorrect_location_format_prompt
+    puts 'Oops! Please use the correct format. First type the column letter and then the row number. For example, a1, or f5'
+  end
+
+  def illegal_location
+    puts 'Sorry! That is not a viable location. Please select again.'
+  end
 end
